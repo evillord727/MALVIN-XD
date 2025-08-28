a87548d2d5e47332b7804df7569dc73383371df8
evillord727 <johnkolawole413@gmail.com>
2025-08-28 15:59:53 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~t6cU3ZzR#Bh0GgdObaDGyFvekUIBuXf7MoPwBqm2RWSsz11QPl4Y",  // add your Session Id here
+  SESSION_ID: "malvin~1i8kSZ7A#Gknk3_PclDqeZm4ItKbqBm8zp3jb5EXEUvpaFrjDJEs",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
